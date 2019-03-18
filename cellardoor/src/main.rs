e1aeb4ca330dfcc3ab1f03dcd6d4f7c46a540c89
Andreas Monitzer <andreas@monitzer.com>
2019-03-18 06:19:42 +0100

    Attempting to integrate a websocket server into http server.

--- cellardoor/src/main.rs
+++ cellardoor/src/main.rs
@@ -3,18 +3,23 @@ use {
     log::{info, debug, error},
     hyper::{
         // Miscellaneous types from Hyper for working with HTTP.
-        Body, Client, Request, Response, Server, Uri, StatusCode, Method,
+        Body, Request, Response, Server, StatusCode, Method,
 
         // This function turns a closure which returns a future into an
         // implementation of the the Hyper `Service` trait, which is an
         // asynchronous function from a generic `Request` to a `Response`.
         service::service_fn,
+        server::conn::AddrStream,
+
+        header::{HeaderValue, UPGRADE, CONTENT_TYPE, CONNECTION, SEC_WEBSOCKET_VERSION, SEC_WEBSOCKET_KEY, SEC_WEBSOCKET_ACCEPT},
+        upgrade::Upgraded,
     },
     futures::{
         // Extension traits providing additional methods on futures.
         // `FutureExt` adds methods that work for all futures, whereas
         // `TryFutureExt` adds methods to futures that return `Result` types.
         future::{FutureExt, TryFutureExt},
+        stream::{Stream, StreamExt},
     },
     std::net::SocketAddr,
 
@@ -24,44 +29,85 @@ use {
         // exposed by `std::future` and implemented by `async fn` syntax).
         await,
         fs::file::File,
+        codec::{Decoder, Framed},
     },
     std::{
         path::{Path, PathBuf},
     },
     mime_guess::get_mime_type_str,
+    websocket::{
+        r#async::{MessageCodec, MsgCodecCtx},
+        message::OwnedMessage,
+    },
 };
 
 mod byte_stream;
 
 const STATIC_FILES: &'static str = "/www";
+const WEBSOCKET_MAGIC: &'static str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+async fn serve_ws(mut framed: Framed<Upgraded, MessageCodec<OwnedMessage>>) {
+    while let (Some(message), new_framed) = await!(framed.into_future()) {
+        debug!("Received message: {:?}", message);
+        framed = new_framed;
+    }
+}
 
 async fn serve_req(req: Request<Body>, mut root: PathBuf) -> Result<Response<Body>, hyper::Error> {
     info!("REQ {} {}", req.method(), req.uri());
     if req.method() == Method::GET {
-        let path = req.uri().path();
-        match path {
-            filename => {
-                root.push(&filename[1..]); // remove leading /
-                let extension = &(Path::new(filename).extension().and_then(|s| s.to_str()));
-                debug!("Requesting file {:?}", root.to_str());
-                match await!(File::open(root.into_boxed_path())) {
-                    Ok(file) => {
-                        let mut response = Response::builder();
-                        if let Some(mimetype) = extension.and_then(|ref extension| get_mime_type_str(&extension)) {
-                            response.header("Content-Type", mimetype);
-                        }
-                        return Ok(response.body(Body::wrap_stream(byte_stream::ByteStream(file))).unwrap());
-                    },
-                    Err(err) => {
-                        error!("{}", err);
-                        return Ok(Response::builder().status(StatusCode::NOT_FOUND).body(Body::from("Not Found")).unwrap());
-                    }
-                }
-            },
-        }
-    }
+        if req.headers().contains_key(UPGRADE) {
+            debug!("Upgrade to websocket!");
 
-    Ok(Response::builder().status(StatusCode::METHOD_NOT_ALLOWED).body(Body::from("Only GET Allowed!")).unwrap())
+            if Some(&HeaderValue::from_static("13")) == req.headers().get(SEC_WEBSOCKET_VERSION) {
+                if let Some(key) = req.headers().get(SEC_WEBSOCKET_KEY) {
+                    let mut hash = sha1::Sha1::new();
+                    hash.update(key.as_bytes());
+                    hash.update(WEBSOCKET_MAGIC.as_bytes());
+                    let accept_str = base64::encode(&hash.digest().bytes());
+
+                    tokio::spawn_async(async move {
+                        if let Ok(upgraded) = await!(req.into_body().on_upgrade()) {
+                            await!(serve_ws(MessageCodec::default(MsgCodecCtx::Server).framed(upgraded)));
+                        } else {
+                            error!("WebSocket upgrade failed.");
+                        }
+                    });
+
+                    Ok(Response::builder().status(StatusCode::SWITCHING_PROTOCOLS)
+                        .header(UPGRADE, "websocket")
+                        .header(CONNECTION, "Upgrade")
+                        .header(SEC_WEBSOCKET_ACCEPT, accept_str)
+                        .body(Body::from("Switching protocols")).unwrap())
+                } else {
+                    Ok(Response::builder().status(StatusCode::BAD_REQUEST).body(Body::from("Missing Sec-WebSocket-Key")).unwrap())
+                }
+            } else {
+                Ok(Response::builder().status(StatusCode::BAD_REQUEST).body(Body::from("Unknown WebSocket version")).unwrap())
+            }
+
+        } else {
+            let filename = req.uri().path();
+            root.push(&filename[1..]); // remove leading /
+            let extension = &(Path::new(filename).extension().and_then(|s| s.to_str()));
+            debug!("Requesting file {:?}", root.to_str());
+            match await!(File::open(root.into_boxed_path())) {
+                Ok(file) => {
+                    let mut response = Response::builder();
+                    if let Some(mimetype) = extension.and_then(|ref extension| get_mime_type_str(&extension)) {
+                        response.header(CONTENT_TYPE, mimetype);
+                    }
+                    Ok(response.body(Body::wrap_stream(byte_stream::ByteStream(file))).unwrap())
+                },
+                Err(err) => {
+                    error!("{}", err);
+                    Ok(Response::builder().status(StatusCode::NOT_FOUND).body(Body::from("Not Found")).unwrap())
+                }
+            }
+        }
+    } else {
+        Ok(Response::builder().status(StatusCode::METHOD_NOT_ALLOWED).body(Body::from("Only GET Allowed!")).unwrap())
+    }
 }
 
 async fn run_server(addr: SocketAddr) {
